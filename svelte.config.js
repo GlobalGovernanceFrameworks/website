905d925d2606f2ccf605ef9dea266f5b89f47771
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-03-23 21:37:31 +0100

    Mirror ConceptExplorer configuration to solve 404 and MIME issues

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,8 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const dev = process.env.NODE_ENV === 'development';
-const base = dev ? '' : '/government-framework-site';
+// IMPORTANT: Change this to match your GitHub repo name
+const base = dev ? '' : '/governance-framework-site';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
