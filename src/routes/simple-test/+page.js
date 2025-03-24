9542857613c41ff12235068276d1948c808e56ed
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-03-24 01:11:10 +0100

    Remove test routes

--- src/routes/simple-test/+page.js
+++
@@ -1,6 +0,0 @@
-// src/routes/simple-test/+page.js
-export function load() {
-  return {
-    content: "# Test Content\n\nThis is a simple test."
-  };
-}
--- src/routes/test/+page.js
+++
@@ -1,2 +0,0 @@
-// src/routes/test/+page.js
-export { default } from '$lib/content/test.md';
