32b2e04f3a62723fdf24b8831b7f04342a699ef5
Björn Kenneth Holmström <bjorn.kenneth.holmstrom@gmail.com>
2025-03-25 02:44:54 +0100

    Fix missing comma in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [sveltekit()],
   build: {
     assetsInclude: ['service-worker.js'] // Ensure service-worker.js is copied to output
-  }
+  },
   server: {
     fs: {
       // Allow serving files from one level up the project root
